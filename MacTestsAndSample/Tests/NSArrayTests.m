9e597c46b1b966604cfa05cf4490bd98f8d3fc4f
Seivan Heidari <seivan@kth.se>
2013-12-17 11:54:45 +0100

    Update.

---
+++ MacTestsAndSample/Tests/NSArrayTests.m
@@ -0,0 +1,534 @@
+//
+//  NSArrayTests.m
+//  Example
+//
+//  Created by Seivan Heidari on 7/22/13.
+//  Copyright (c) 2013 Seivan Heidari. All rights reserved.
+//
+
+
+#import "SHFastEnumerationTests.h"
+
+#import "NSArray+SHFastEnumerationProtocols.h"
+
+
+
+
+
+@interface NSArrayTests : XCTestCase
+<SHTestsFastEnumerationBlocks,
+SHTestsFastEnumerationProperties,
+SHTestsFastEnumerationOrderedBlocks,
+SHTestsFastEnumerationOrderedProperties,
+SHTestsFastEnumerationOrdered>
+
+@property(nonatomic,strong) NSArray             * subject;
+@property(nonatomic,strong) NSMutableArray      * matching;
+
+@end
+
+@interface NSArrayTests (Mutable)
+<SHTestsMutableFastEnumerationBlocks,
+SHTestsMutableFastEnumerationOrdered>
+@end
+
+@interface NSArrayTests (Private)
+<SHTestsHelpers>
+@end
+
+
+@implementation NSArrayTests
+
+
+-(void)setUp; {
+  [super setUp];
+  self.subject = @[ @"one", @"1", @"two",@"2", @"three", @"3", @"one", @"1"];
+  self.matching = @[].mutableCopy;
+}
+
+-(void)tearDown; {
+  [super tearDown];
+  
+  self.subject = nil;
+  self.matching = nil;
+}
+
+#pragma mark - <SHTestsFastEnumerationBlocks>
+-(void)testEach;{
+  [self.subject SH_each:^(id obj) {
+    [self.matching addObject:obj];
+  }];
+  
+  XCTAssertEqualObjects(self.subject, self.matching);
+  
+}
+
+-(void)testConcurrentEach; {
+  __block BOOL didAssert = NO;
+  NSMutableArray * content = @[].mutableCopy;
+  [self SH_performAsyncTestsWithinBlock:^(BOOL *didFinish) {
+    [self.subject SH_concurrentEach:^(id obj) {
+      [content addObject:obj];
+    } onComplete:^(id obj) {
+      XCTAssertEqualObjects(obj, self.subject);
+      didAssert = YES;
+      *didFinish = YES;
+    }];
+  } withTimeout:5];
+  
+  XCTAssertTrue(didAssert);
+  XCTAssertEqual(content.count, self.subject.count);
+}
+
+-(void)testMap;{
+  __block NSInteger skipOne = -1;
+  self.matching = [self.subject SH_map:^id(id obj) {
+    skipOne += 1;
+    if(skipOne == 0)
+      return nil;
+    else
+      return obj;
+  }].mutableCopy;
+  
+  NSMutableArray * modifySubject =  self.subject.mutableCopy;
+  [modifySubject removeObjectAtIndex:0];
+  self.subject = modifySubject;
+
+  XCTAssertEqualObjects(self.subject, self.matching);
+  
+}
+
+-(void)testReduce;{
+  self.matching = [self.subject SH_reduceValue:@[].mutableCopy
+                                     withBlock:^id(NSMutableArray * memo, id obj) {
+    [memo addObject:obj];
+    return memo;
+  }];
+
+  XCTAssertEqualObjects(self.subject, self.matching);
+}
+
+-(void)testFind;{
+  NSInteger index = self.subject.count/2;
+  
+  id value = [self.subject SH_find:^BOOL(id obj) {
+    return [self.subject indexOfObject:obj] == index;
+  }];
+  [self.matching addObject:value];
+
+  XCTAssertEqualObjects(self.subject[index], self.matching[0]);
+  
+}
+
+-(void)testFindAll;{
+  self.matching = [self.subject SH_findAll:^BOOL(id obj) {
+    return [self.subject indexOfObject:obj] % 2 == 0 ;
+  }].mutableCopy;
+
+  for (id obj in self.matching) {
+    NSInteger index = [self.subject indexOfObject:obj];
+    XCTAssertTrue([self.subject containsObject:obj]);
+    XCTAssertTrue(index % 2 == 0);
+  }
+  XCTAssertTrue(self.matching.count > 0);
+
+}
+
+-(void)testReject;{
+  self.matching = [self.subject SH_reject:^BOOL(id obj) {
+    return [self.subject indexOfObject:obj] % 2 == 0 ;
+  }].mutableCopy;
+
+  for (id obj in self.matching) {
+    NSInteger index = [self.subject indexOfObject:obj];
+    XCTAssertTrue(index % 2 != 0);
+  }
+  XCTAssertTrue(self.matching.count < self.subject.count);
+  XCTAssertTrue(self.matching.count > 0);
+}
+
+-(void)testAll;{
+  self.matching = self.subject.mutableCopy;
+  BOOL testAllTrue = [self.subject SH_all:^BOOL(id obj) {
+    return [self.matching containsObject:obj];
+  }];
+  
+  [self.matching removeLastObject];
+  [self.matching removeLastObject];
+  [self.matching removeLastObject];
+  BOOL testAllNotAllTrue = [self.subject SH_all:^BOOL(id obj) {
+    return [self.matching containsObject:obj];
+  }];
+  
+  self.matching = @[].mutableCopy;
+  if(testAllTrue) [self.matching addObject:@(1)];
+  if(testAllNotAllTrue == NO) [self.matching addObject:@(2)];
+
+  NSArray * matching = (NSArray * )self.matching;
+  XCTAssertTrue([matching containsObject:@(1)]);
+  XCTAssertTrue([matching containsObject:@(2)]);
+
+
+
+}
+
+-(void)testAny;{
+  self.matching = self.subject.mutableCopy;
+  BOOL testAllTrue = [self.subject SH_any:^BOOL(id obj) {
+    return [self.matching containsObject:obj];
+  }];
+  
+  [self.matching removeLastObject];
+  [self.matching removeLastObject];
+  [self.matching removeLastObject];
+
+  
+  BOOL testAllNotAllTrue = [self.subject SH_any:^BOOL(id obj) {
+    return [self.matching containsObject:obj];
+  }];
+
+  BOOL testAllAllFalse = [self.subject SH_any:^BOOL(id obj) {
+    return NO;
+  }];
+
+  self.matching = @[].mutableCopy;
+  
+  if(testAllTrue) [self.matching addObject:@(1)];
+  if(testAllNotAllTrue) [self.matching addObject:@(2)];
+  if(testAllAllFalse == NO) [self.matching addObject:@(3)];
+  
+  NSArray * matching = (NSArray * )self.matching;
+  
+  XCTAssertTrue([matching containsObject:@(1)]);
+  
+  XCTAssertTrue([matching containsObject:@(2)]);
+  
+  XCTAssertTrue([matching containsObject:@(3)]);
+
+
+
+
+}
+
+-(void)testNone; {
+  self.matching = self.subject.mutableCopy;
+  
+  BOOL testAllTrue = [self.subject SH_none:^BOOL(id obj) {
+    return NO;
+  }];
+
+  BOOL testAllFalse = [self.subject SH_none:^BOOL(id obj) {
+    return YES;
+  }];
+
+  self.matching = @[].mutableCopy;
+  
+  if(testAllTrue) [self.matching addObject:@(1)];
+  if(testAllFalse == NO) [self.matching addObject:@(2)];
+
+  NSArray * matching = (NSArray * )self.matching;
+  XCTAssertTrue([matching containsObject:@(1)]);
+  XCTAssertTrue([matching containsObject:@(2)]);
+
+  
+}
+
+#pragma mark - <SHTestsFastEnumerationProperties>
+-(void)testIsEmtpy; {
+  XCTAssertFalse(self.subject.SH_isEmpty);
+  XCTAssertTrue(self.matching.SH_isEmpty);
+  BOOL isEmpty = self.matching.count == 0;
+  XCTAssertEqual(isEmpty, self.matching.SH_isEmpty);
+}
+-(void)testToArray; {
+  self.matching = self.subject.SH_toArray.copy;
+  
+  XCTAssertTrue([self.matching isKindOfClass:[NSArray class]]);
+  XCTAssertEqualObjects(self.subject, self.matching);
+}
+
+-(void)testToSet; {
+  XCTAssertTrue([self.subject.SH_toSet isKindOfClass:[NSSet class]]);
+  XCTAssertTrue(self.subject.SH_toSet.count > 0);
+  
+  for (id obj in self.subject.SH_toSet)
+    XCTAssertTrue([self.subject containsObject:obj]);
+
+}
+
+-(void)testToOrderedSet; {
+  XCTAssertTrue([self.subject.SH_toOrderedSet  isKindOfClass:[NSOrderedSet class]]);
+  XCTAssertTrue(self.subject.SH_toOrderedSet.count > 0);
+  
+  [self.subject.SH_toOrderedSet enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *_) {
+    XCTAssertEqualObjects(obj, self.subject[idx]);
+  }];
+    
+
+}
+
+-(void)testToDictionary; {
+  XCTAssertTrue([self.subject.SH_toDictionary isKindOfClass:[NSDictionary class]]);
+  XCTAssertTrue(self.subject.SH_toDictionary.count > 0);
+  
+  [self.subject SH_eachWithIndex:^(id obj, NSInteger index) {
+    XCTAssertEqualObjects(self.subject[index], [self.subject.SH_toDictionary objectForKey:@(index)]);
+  }];
+
+}
+
+-(void)testToMapTableWeakToWeak; {
+  [self assertMapTableWithMapTable:self.subject.SH_toMapTableWeakToWeak];
+  
+}
+
+-(void)testToMapTableWeakToStrong; {
+  [self assertMapTableWithMapTable:self.subject.SH_toMapTableWeakToStrong];
+}
+
+-(void)testToMapTableStrongToStrong; {
+  [self assertMapTableWithMapTable:self.subject.SH_toMapTableStrongToStrong];
+}
+
+-(void)testToMapTableStrongToWeak; {
+  [self assertMapTableWithMapTable:self.subject.SH_toMapTableStrongToWeak];
+}
+
+-(void)testToHashTableWeak; {
+  [self assertHashTableWithMapTable:self.subject.SH_toHashTableWeak];
+}
+
+-(void)testToHashTableStrong; {
+  [self assertHashTableWithMapTable:self.subject.SH_toHashTableStrong];
+}
+
+-(void)testAvg; {
+  self.matching = @[@(0),@(1),@(2),@(3),@(4),@(5)].mutableCopy;
+  self.subject  = @[@"0",@"1",@"2",@"3",@"4",@"5"];
+  XCTAssertEqualObjects([self.subject valueForKeyPath:@"@avg.self"],
+                       self.matching.SH_collectionAvg);
+  
+
+}
+
+-(void)testSum; {
+  self.matching = @[@(0),@(1),@(2),@(3),@(4),@(5)].mutableCopy;
+  self.subject  = @[@"0",@"1",@"2",@"3",@"4",@"5"];
+  XCTAssertEqualObjects([self.subject valueForKeyPath:@"@sum.self"],
+                       self.matching.SH_collectionSum);
+
+  
+}
+
+-(void)testMax; {
+  self.matching = @[@(0),@(1),@(2),@(3),@(4),@(5)].mutableCopy;
+  self.subject  = @[@"0",@"1",@"2",@"3",@"4",@"5"];
+  
+  XCTAssertEqualObjects([self.subject valueForKeyPath:@"@max.self"], self.subject.SH_collectionMax);
+  XCTAssertEqualObjects([self.matching valueForKeyPath:@"@max.self"], self.matching.SH_collectionMax);
+
+}
+
+-(void)testMin; {
+  self.matching = @[@(0),@(1),@(2),@(3),@(4),@(5)].mutableCopy;
+  self.subject  = @[@"0",@"1",@"2",@"3",@"4",@"5"];
+  
+  XCTAssertEqualObjects([self.subject valueForKeyPath:@"@min.self"], self.subject.SH_collectionMin);
+  XCTAssertEqualObjects([self.matching valueForKeyPath:@"@min.self"], self.matching.SH_collectionMin);
+}
+
+
+#pragma mark - <SHTestsFastEnumerationOrderedBlocks>
+-(void)testEachWithIndex;{
+  
+  [self.subject SH_eachWithIndex:^(id obj, NSInteger index) {
+    [self.matching addObject:@(index)];
+  }];
+  
+  XCTAssertTrue(self.matching.count > 0);
+  for (NSNumber * obj in self.matching)
+    XCTAssertNotNil(self.subject[obj.unsignedIntegerValue]);
+  
+  
+  
+  
+}
+
+
+#pragma mark - <SHTestsFastEnumerationOrderedProperties>
+-(void)testFirstObject; {
+  self.matching = self.subject.mutableCopy;
+  
+  XCTAssertEqualObjects(self.matching.SH_firstObject,
+                       self.subject[0]);
+  
+  self.matching = @[].mutableCopy;
+  
+  XCTAssertNoThrow([self.matching SH_firstObject]);
+  XCTAssertNil(self.matching.SH_firstObject);
+  
+}
+
+-(void)testLastObject; {
+  self.matching = self.subject.mutableCopy;
+  
+  XCTAssertEqualObjects(self.matching.SH_lastObject,
+                       self.subject.lastObject);
+  
+  self.matching = @[].mutableCopy;
+  
+  XCTAssertNoThrow([self.matching SH_lastObject]);
+  XCTAssertNil(self.matching.SH_lastObject);
+  
+  
+}
+
+#pragma mark - <SHTestsFastEnumerationOrdered>
+-(void)testReverse; {
+
+  self.matching = [self.subject SH_reverse].mutableCopy;
+  XCTAssertEqualObjects(self.subject.reverseObjectEnumerator.allObjects, self.matching);
+  XCTAssertTrue(self.matching.count > 0);
+
+  
+}
+
+#pragma mark - <SHTestsMutableFastEnumerationBlocks>
+-(void)testModifyMap; {
+  __block NSInteger counter = 0;
+  self.matching = self.subject.mutableCopy;
+  [self.matching SH_modifyMap:^id(id obj) {
+    counter +=1;
+    if(counter == 1)
+      return obj;
+    else
+      return nil;
+  }];
+  
+  
+  XCTAssertTrue(self.matching.count < self.subject.count);
+  XCTAssertTrue(self.matching.count == 1);
+  XCTAssertEqualObjects(self.matching[0], self.subject[0]);
+  
+
+}
+
+-(void)testModifyFindAll; {
+  self.matching = self.subject.mutableCopy;
+  
+  [self.matching SH_modifyFindAll:^BOOL(id obj) {
+    return [self.matching indexOfObject:obj] % 2 == 0 ;
+  }];
+  
+  for (id obj in self.matching) {
+    NSInteger index = [self.subject indexOfObject:obj];
+    XCTAssertTrue(index % 2 == 0);
+  }
+  XCTAssertTrue(self.matching.count < self.subject.count);
+  XCTAssertTrue(self.matching.count > 0);
+  
+}
+
+-(void)testModifyReject; {
+
+  self.matching = self.subject.mutableCopy;
+  
+  [self.matching SH_modifyReject:^BOOL(id obj) {
+    return [self.matching indexOfObject:obj] % 2 == 0 ;
+  }];
+  
+
+  for (id obj in self.matching) {
+    NSInteger index = [self.subject indexOfObject:obj];
+    XCTAssertTrue(index % 2 != 0);
+  }
+  XCTAssertTrue(self.matching.count < self.subject.count);
+  XCTAssertTrue(self.matching.count > 0);
+}
+
+#pragma mark - <SHTestsMutableFastEnumerationOrdered>
+
+-(void)testModifyReverse; {
+  self.matching = self.subject.mutableCopy;
+  [self.matching SH_modifyReverse];
+  XCTAssertEqualObjects(self.subject.reverseObjectEnumerator.allObjects, self.matching);
+  XCTAssertTrue(self.matching.count > 0);
+}
+
+-(void)testPopObjectAtIndex; {
+  self.matching = self.subject.mutableCopy;
+  id obj = [self.matching SH_popObjectAtIndex:self.matching.count/2];
+  
+  XCTAssertEqualObjects(obj,
+                       self.subject[self.subject.count/2]);
+
+  XCTAssertTrue(self.matching.count == self.subject.count-1 );
+  
+  
+//  XCTAssertThrowsSpecific([self.matching SH_popObjectAtIndex:NSNotFound],
+//                         NSException,
+//                         nil);
+}
+
+-(void)testPopFirstObject; {
+  self.matching = self.subject.mutableCopy;
+  id obj = [self.matching SH_popFirstObject];
+
+  XCTAssertNotNil(obj);
+  XCTAssertEqualObjects(obj,
+                       self.subject.SH_firstObject);
+  
+  XCTAssertTrue(self.matching.count == self.subject.count-1 );
+  
+  self.matching = @[].mutableCopy;
+  
+  XCTAssertNoThrow([self.matching SH_popFirstObject]);
+  XCTAssertNil([self.matching SH_popFirstObject]);
+
+}
+
+-(void)testPopLastObject; {
+  self.matching = self.subject.mutableCopy;
+  id obj = [self.matching SH_popLastObject];
+  
+  XCTAssertEqualObjects(obj,
+                       self.subject.SH_lastObject);
+  
+  XCTAssertTrue(self.matching.count == self.subject.count-1 );
+  
+  self.matching = @[].mutableCopy;
+  
+  XCTAssertNoThrow([self.matching SH_lastObject]);
+  XCTAssertNil([self.matching SH_lastObject]);
+
+  
+}
+
+@end
+
+
+@implementation NSArrayTests (Private)
+-(void)assertMapTableWithMapTable:(NSMapTable *)theMapTable; {
+  
+  XCTAssertTrue([theMapTable isKindOfClass:[NSMapTable class]]);
+  XCTAssertTrue(theMapTable.count > 0);
+  XCTAssertTrue(self.subject.count > 0);
+  
+  [self.subject SH_eachWithIndex:^(id obj, NSInteger index) {
+    XCTAssertEqualObjects(self.subject[index], [theMapTable objectForKey:@(index)]);
+  }];
+
+}
+
+-(void)assertHashTableWithMapTable:(NSHashTable *)theHashTable; {
+  XCTAssertTrue([theHashTable isKindOfClass:[NSHashTable class]]);
+  XCTAssertTrue(theHashTable.count > 0);
+  XCTAssertTrue(self.subject.count > 0);
+  [self.subject SH_each:^(id obj) {
+    XCTAssertTrue([theHashTable containsObject:obj]);
+  }];
+  
+  
+}
+
+@end
---
+++ MacTestsAndSample/Tests/NSSetTests.m
@@ -0,0 +1,434 @@
+//
+//  NSSetTests.m
+//  Example
+//
+//  Created by Seivan Heidari on 7/25/13.
+//  Copyright (c) 2013 Seivan Heidari. All rights reserved.
+//
+
+#import "SHFastEnumerationTests.h"
+
+#import "NSSet+SHFastEnumerationProtocols.h"
+
+
+@interface NSSetTests : XCTestCase
+<SHTestsFastEnumerationBlocks,
+SHTestsFastEnumerationProperties>
+@property(nonatomic,strong) NSSet           * subject;
+@property(nonatomic,strong) NSMutableSet    * matching;
+
+@end
+
+
+@interface NSSetTests (Private)
+<SHTestsHelpers>
+@end
+
+@interface NSSetTests (Mutable)
+<SHTestsMutableFastEnumerationBlocks>
+@end
+
+@implementation NSSetTests
+
+
+-(void)setUp; {
+  [super setUp];
+  
+  
+  self.subject =  [NSSet setWithArray:@[@"one", @"1", @"two",@"2", @"three", @"3", @"one", @"1"]];
+  
+  self.matching = [NSMutableSet set];
+}
+
+-(void)tearDown; {
+  [super tearDown];
+  
+  self.subject = nil;
+  self.matching = nil;
+}
+
+#pragma mark - <SHTestsFastEnumerationBlocks>
+-(void)testEach;{
+  [self.subject SH_each:^(id obj) {
+    [self.matching addObject:obj];
+  }];
+  
+  XCTAssertEqualObjects(self.subject, self.matching);
+  
+}
+
+-(void)testConcurrentEach; {
+  __block BOOL didAssert = NO;
+  NSMutableArray * content = @[].mutableCopy;
+  [self SH_performAsyncTestsWithinBlock:^(BOOL *didFinish) {
+    [self.subject SH_concurrentEach:^(id obj) {
+      [content addObject:obj];
+    } onComplete:^(id obj) {
+      XCTAssertEqualObjects(obj, self.subject);
+      didAssert = YES;
+      *didFinish = YES;
+    }];
+  } withTimeout:5];
+  
+  XCTAssertTrue(didAssert);
+  XCTAssertEqual(content.count, self.subject.count);
+}
+
+
+-(void)testMap;{
+  __block NSInteger skipOne = 0;
+  self.matching = [self.subject SH_map:^id(id obj) {
+    skipOne += 1;
+    if(skipOne == 1)
+      return nil;
+    else
+      return obj;
+  }].mutableCopy;
+  
+  self.subject = [self.subject SH_map:^id(id obj) {
+    return obj;
+  }];
+  
+  
+  XCTAssertTrue(self.matching.count < self.subject.count);
+  XCTAssertFalse(self.matching.SH_isEmpty);
+  
+  for (id obj in self.matching)
+    XCTAssertTrue([self.subject containsObject:obj]);
+  
+}
+
+-(void)testReduce;{
+  NSMutableString * expected = @"".mutableCopy;
+  for (id obj in self.subject) [expected appendFormat:@"%@", obj];
+  
+  NSMutableString  * matched = [self.subject SH_reduceValue:@"".mutableCopy withBlock:^id(NSMutableString * memo, id obj) {
+    [memo appendFormat:@"%@", obj];
+    return memo;
+  }];
+  
+  XCTAssertEqualObjects(expected, matched);
+}
+
+-(void)testFind;{
+  __block NSInteger counter = 0;
+  
+  id value = [self.subject SH_find:^BOOL(id obj) {
+    counter +=1;
+    return (counter == self.subject.count);
+  }];
+  
+  
+  XCTAssertEqual(self.subject.count, (NSUInteger)counter);
+  XCTAssertTrue([self.subject containsObject:value]);
+  
+}
+
+-(void)testFindAll;{
+  __block NSInteger counter = 0;
+  
+  self.matching = [self.subject SH_findAll:^BOOL(id obj) {
+    counter +=1;
+    return (counter < self.subject.count-1);
+  }].mutableCopy;
+  
+  for (id obj in self.matching) {
+    XCTAssertTrue([self.subject containsObject:obj]);
+  }
+  XCTAssertTrue(self.matching.count > 0);
+  XCTAssertTrue(self.matching.count < self.subject.count-1);
+  
+}
+
+-(void)testReject;{
+  __block NSInteger counter = 0;
+  
+  self.matching = [self.subject SH_reject:^BOOL(id obj) {
+    counter +=1;
+    return (counter < self.subject.count-1);
+  }].mutableCopy;
+  
+  for (id obj in self.matching) {
+    XCTAssertTrue([self.subject containsObject:obj]);
+  }
+  XCTAssertTrue(self.matching.count > 0);
+  XCTAssertTrue(self.matching.count < self.subject.count-1);
+}
+
+-(void)testAll;{
+  
+  self.matching = self.subject.mutableCopy;
+  BOOL testAllTrue = [self.subject SH_all:^BOOL(id obj) {
+    return [self.matching containsObject:obj];
+  }];
+  
+  NSMutableSet * subject = self.subject.mutableCopy;
+  [subject addObject:@"---"];
+  self.subject = subject.copy;
+  
+  BOOL testAllNotAllTrue = [self.subject SH_all:^BOOL(id obj) {
+    return [self.matching containsObject:obj];
+  }];
+  
+  XCTAssertTrue(testAllTrue);
+  XCTAssertFalse(testAllNotAllTrue);
+  
+  
+}
+
+-(void)testAny;{
+  self.matching = self.subject.mutableCopy;
+  
+  BOOL testAllTrue = [self.subject SH_any:^BOOL(id obj) {
+    return [self.matching containsObject:obj];
+  }];
+  
+  
+  NSMutableArray * matching = self.matching.SH_toArray.mutableCopy;
+  [matching removeLastObject];
+  [matching removeLastObject];
+  BOOL testAllNotAllTrue = [self.subject SH_any:^BOOL(id obj) {
+    return [matching containsObject:obj];
+  }];
+  
+  XCTAssertTrue(testAllTrue);
+  XCTAssertTrue(testAllNotAllTrue);
+  
+  
+  
+  
+}
+
+-(void)testNone; {
+  self.matching = self.subject.mutableCopy;
+  
+  BOOL testAllTrue = [self.subject SH_none:^BOOL(id obj) {
+    return NO;
+  }];
+  
+  BOOL testAllFalse = [self.subject SH_none:^BOOL(id obj) {
+    return YES;
+  }];
+  
+  self.matching = @[].mutableCopy;
+  
+  if(testAllTrue) [self.matching addObject:@(1)];
+  if(testAllFalse == NO) [self.matching addObject:@(2)];
+  
+  NSArray * matching = (NSArray * )self.matching;
+  XCTAssertTrue([matching containsObject:@(1)]);
+  XCTAssertTrue([matching containsObject:@(2)]);
+  
+  
+}
+
+#pragma mark - <SHTestsFastEnumerationProperties>
+-(void)testIsEmtpy; {
+  XCTAssertFalse(self.subject.SH_isEmpty);
+  XCTAssertTrue(self.matching.SH_isEmpty);
+  BOOL isEmpty = self.matching.count == 0;
+  XCTAssertEqual(isEmpty, self.matching.SH_isEmpty);
+}
+
+-(void)testToArray; {
+  NSArray     * matching = self.subject.SH_toArray;
+  NSArray     * subject  = self.subject.allObjects;
+  
+  XCTAssertTrue([matching isKindOfClass:[NSArray class]]);
+  XCTAssertEqualObjects(subject, matching);
+}
+
+-(void)testToSet; {
+  XCTAssertTrue([self.subject.SH_toSet isKindOfClass:[NSSet class]]);
+  XCTAssertTrue(self.subject.SH_toSet.count > 0);
+  
+  for (id obj in self.subject.SH_toSet)
+    XCTAssertTrue([self.subject containsObject:obj]);
+  
+}
+
+-(void)testToOrderedSet; {
+  XCTAssertTrue([self.subject.SH_toOrderedSet  isKindOfClass:[NSOrderedSet class]]);
+  XCTAssertTrue(self.subject.SH_toOrderedSet.count > 0);
+  
+  [self.subject.SH_toOrderedSet enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *_) {
+    XCTAssertTrue([self.subject containsObject:obj]);
+  }];
+  
+  
+}
+
+-(void)testToDictionary; {
+  XCTAssertTrue([self.subject.SH_toDictionary isKindOfClass:[NSDictionary class]]);
+  XCTAssertTrue(self.subject.SH_toDictionary.count > 0);
+  
+  [self.subject.SH_toDictionary enumerateKeysAndObjectsUsingBlock:^(NSNumber * key, id obj, BOOL *stop) {
+    [self.subject containsObject:obj];
+    XCTAssertTrue([self.subject containsObject:obj]);
+    XCTAssertNotNil(self.subject.objectEnumerator.allObjects[key.integerValue]);
+  }];
+  
+}
+
+-(void)testToMapTableWeakToWeak; {
+  [self assertMapTableWithMapTable:self.subject.SH_toMapTableWeakToWeak];
+  
+}
+
+-(void)testToMapTableWeakToStrong; {
+  [self assertMapTableWithMapTable:self.subject.SH_toMapTableWeakToStrong];
+}
+
+-(void)testToMapTableStrongToStrong; {
+  [self assertMapTableWithMapTable:self.subject.SH_toMapTableStrongToStrong];
+}
+
+-(void)testToMapTableStrongToWeak; {
+  [self assertMapTableWithMapTable:self.subject.SH_toMapTableStrongToWeak];
+}
+
+-(void)testToHashTableWeak; {
+  [self assertHashTableWithMapTable:self.subject.SH_toHashTableWeak];
+}
+
+-(void)testToHashTableStrong; {
+  [self assertHashTableWithMapTable:self.subject.SH_toHashTableStrong];
+}
+
+-(void)testAvg; {
+  self.matching = [NSMutableSet setWithArray:@[@(0),@(1),@(2),@(3),@(4),@(5)]];
+  self.subject  = [NSSet setWithArray:@[@"0",@"1",@"2",@"3",@"4",@"5"]];
+  XCTAssertEqualObjects([self.subject valueForKeyPath:@"@avg.self"],
+                       self.matching.SH_collectionAvg);
+}
+
+-(void)testSum; {
+  self.matching = [NSMutableSet setWithArray:@[@(0),@(1),@(2),@(3),@(4),@(5)]];
+  self.subject  = [NSSet setWithArray:@[@"0",@"1",@"2",@"3",@"4",@"5"]];
+  XCTAssertEqualObjects([self.subject valueForKeyPath:@"@sum.self"],
+                       self.matching.SH_collectionSum);
+  
+}
+
+-(void)testMax; {
+  self.matching = [NSMutableSet setWithArray:@[@(0),@(1),@(2),@(3),@(4),@(5)]];
+  self.subject  = [NSSet setWithArray:@[@"0",@"1",@"2",@"3",@"4",@"5"]];
+  
+  XCTAssertEqualObjects([self.subject valueForKeyPath:@"@max.self"], self.subject.SH_collectionMax);
+  XCTAssertEqualObjects([self.matching valueForKeyPath:@"@max.self"], self.matching.SH_collectionMax);
+  
+}
+
+-(void)testMin; {
+  self.matching = [NSMutableSet setWithArray:@[@(0),@(1),@(2),@(3),@(4),@(5)]];
+  self.subject  = [NSSet setWithArray:@[@"0",@"1",@"2",@"3",@"4",@"5"]];
+  
+  XCTAssertEqualObjects([self.subject valueForKeyPath:@"@min.self"], self.subject.SH_collectionMin);
+  XCTAssertEqualObjects([self.matching valueForKeyPath:@"@min.self"], self.matching.SH_collectionMin);
+}
+
+
+@end
+
+@implementation NSSetTests (Mutable)
+
+#pragma mark - <SHTestsMutableFastEnumerationBlocks>
+-(void)testModifyMap; {
+  __block NSInteger counter = 0;
+  self.matching = self.subject.mutableCopy;
+  [self.matching SH_modifyMap:^id(id obj) {
+    counter +=1;
+    if(counter == 1)
+      return obj;
+    else
+      return nil;
+  }];
+  
+  
+  NSInteger expectedCount = 1;
+  XCTAssertTrue(self.matching.count < self.subject.count);
+  XCTAssertEqual(self.matching.count, (NSUInteger)expectedCount);
+  
+  for (id obj in self.matching) {
+    XCTAssertTrue([self.subject containsObject:obj]);
+  }
+  
+  
+  
+}
+
+-(void)testModifyFindAll; {
+  __block NSInteger counter = 0;
+  self.matching = self.subject.mutableCopy;
+  [self.matching SH_modifyFindAll:^BOOL(id obj) {
+    counter +=1;
+    if(counter == 1)
+      return YES;
+    else
+      return NO;
+  }];
+  
+  
+  NSInteger expectedCount = 1;
+  XCTAssertTrue(self.matching.count < self.subject.count);
+  XCTAssertEqual(self.matching.count, (NSUInteger)expectedCount);
+  
+  for (id obj in self.matching) {
+    XCTAssertTrue([self.subject containsObject:obj]);
+  }
+  
+  
+  
+}
+
+-(void)testModifyReject; {
+  
+  __block NSInteger counter = 0;
+  self.matching = self.subject.mutableCopy;
+  [self.matching SH_modifyReject:^BOOL(id obj) {
+    counter +=1;
+    if(counter == 1)
+      return YES;
+    else
+      return NO;
+  }];
+  
+  
+  NSInteger expectedCount = 1;
+  XCTAssertTrue(self.matching.count < self.subject.count);
+  XCTAssertEqual(self.matching.count, self.subject.count-expectedCount);
+  
+  for (id obj in self.matching) {
+    XCTAssertTrue([self.subject containsObject:obj]);
+  }
+}
+
+
+@end
+
+
+@implementation NSSetTests (Private)
+-(void)assertMapTableWithMapTable:(NSMapTable *)theMapTable; {
+  
+  XCTAssertTrue([theMapTable isKindOfClass:[NSMapTable class]]);
+  XCTAssertTrue(theMapTable.count > 0);
+  XCTAssertTrue(self.subject.count > 0);
+  [self.subject SH_each:^(id obj) {
+    [theMapTable.objectEnumerator.allObjects containsObject:obj];
+  }];
+  
+}
+
+
+-(void)assertHashTableWithMapTable:(NSHashTable *)theHashTable; {
+  XCTAssertTrue([theHashTable isKindOfClass:[NSHashTable class]]);
+  XCTAssertTrue(theHashTable.count > 0);
+  XCTAssertTrue(self.subject.count > 0);
+  [self.subject SH_each:^(id obj) {
+    XCTAssertTrue([theHashTable containsObject:obj]);
+  }];
+  
+}
+
+
+@end
